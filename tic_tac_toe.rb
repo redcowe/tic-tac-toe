15f1e9d92b33ce8fce072e19af676f4c58ccb3a2
redcowe <joshuacowell@hotmail.com>
2021-12-29 13:20:35 -0500

    Started sudo code

---
+++ tic_tac_toe.rb
@@ -0,0 +1,21 @@
+#########################
+
+# Two players go back and forth on per turn
+
+# Winning is only possible after a minimum of three turns
+
+# There is a maximum of 9 turns that can be played
+
+# 
+#
+#
+#
+#
+
+#########################
+
+board = [
+    [0,0][0,0][0,0]
+    [0,0][0,0][0,0]
+    [0,0][0,0][0,0]
+]
