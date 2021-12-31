b1af2f880f0504e309cc7b180e13184d28452bc5
redcowe <joshuacowell@hotmail.com>
2021-12-30 20:19:35 -0500

    Added some functionality, continued psudeo code.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,8 @@
 #########################
 
 # Two players go back and forth on per turn
+# Create a player class that creates each player
+# Player one will be in the 
 
 # Winning is only possible after a minimum of three turns
 
@@ -14,8 +16,49 @@
 
 #########################
 
-board = [
-    [0,0][0,0][0,0]
-    [0,0][0,0][0,0]
-    [0,0][0,0][0,0]
-]
+winner = false
+
+# Creates a player object
+class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# Creates game board
+class Board
+  def self.create_board
+    board = [
+      [0], [0], [0],
+      [0], [0], [0],
+      [0], [0], [0]
+    ]
+  end
+end
+
+# Responible for creating and managing the game
+module Game
+  @number_of_players = 0
+  @number_of_turns = 0
+
+  def self.play_game
+    # create players
+    if @number_of_players.zero?
+      print 'Player one please enter your name: '
+      player_one = Player.new(gets.chomp)
+      puts "Player one: #{player_one.name}"
+      @number_of_players += 1
+    else
+      print 'Player two please enter your name: '
+      player_two = Player.new(gets.chomp)
+      puts "Player two: #{player_two.name}"
+      @number_of_players += 1
+    end
+
+    # create the board
+  end
+end
+
+Game.play_game until winner
